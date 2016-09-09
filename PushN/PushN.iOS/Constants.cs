19a802e0f961768436b185181d3b89983d3bf02c
AkshayK <akshay@quexst.com>
2016-09-09 15:45:25 +0530

    mistakenly forgot the google api key ther which is not required in iOS project

--- PushN/PushN.iOS/Constants.cs
+++ PushN/PushN.iOS/Constants.cs
@@ -8,7 +8,7 @@ namespace PushN.iOS
     {
         // Azure app-specific connection string and hub path
 
-        public const string SenderID = "Google-Project-Number"; // Google API Project Number
+       
         public const string ConnectionString = "Notification-hub-connection-string";
         public const string NotificationHubPath = "Notification-Hub-Name";
     }
